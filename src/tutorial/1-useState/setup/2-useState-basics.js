19700ec9183f9f79a990097a31de0f797a925459
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-05-09 22:24:16 +0200

    useState function
    useState is a function that comes from React.
    This is a named import so the names must match.
    [0] is the value you want to pass in.
    [1] is the function to handle the value.
    Naming convention [value, setValue] on line 8

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,24 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  // console.log("useState:", useState());
+  // const value = useState(1)[0];
+  // // const handler = useState(1)[1];
+  // console.log("value:", value, "handler:", handler);
+  const [text, setText] = useState("Random Title"); //  Destructuring Array
+  const clickHandler = () => {
+    if (text === "Random Title") {
+      setText("Hello World")
+    } else {
+      setText("Random Title")
+    }
+  }
+  return (
+    <React.Fragment>
+      <h2>{text}</h2>
+      <button className="btn" onClick={clickHandler}>Change Title </button>
+    </React.Fragment>
+  )
 };
 
 export default UseStateBasics;
