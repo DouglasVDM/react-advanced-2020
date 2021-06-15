39077ee1a46e713250913e9ed98f61ed69757fe4
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-06-15 16:34:00 +0200

    basic hook rules

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,10 +1,15 @@
 import React, { useState } from 'react';
+// Hook will start with use
+// component name must be uppercase
+// hook must be inside the function body
+// we cannot call the hook conditionally(inside an if)
 
 const UseStateBasics = () => {
   // console.log("useState:", useState());
   // const value = useState(1)[0];
-  // // const handler = useState(1)[1];
+  // const handler = useState(1)[1];
   // console.log("value:", value, "handler:", handler);
+
   const [text, setText] = useState("Random Title"); //  Destructuring Array
   const clickHandler = () => {
     if (text === "Random Title") {
